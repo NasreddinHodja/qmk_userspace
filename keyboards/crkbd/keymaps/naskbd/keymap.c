717ade330ca42ca0ac6c1c294be88ebf6a3b5383
tbizetde@gmail.com <tbizetde@gmail.com>
2025-12-03 11:44:10 -0300

    naskbd-mirocu as standard

--- keyboards/crkbd/keymaps/naskbd/keymap.c
+++ keyboards/crkbd/keymaps/naskbd/keymap.c
@@ -15,15 +15,6 @@
 #define HRSFT(key) MT(MOD_LSFT, key)
 #define HRAGR(key) MT(MOD_RALT, key)
 
-#define SYM_LGUI LGUI_T(KC_0)
-#define SYM_LALT LALT_T(KC_1)
-#define SYM_LCTL LCTL_T(KC_2)
-#define SYM_LSFT LSFT_T(KC_3)
-#define SYM_RGUI LGUI_T(KC_4)
-#define SYM_RALT LALT_T(KC_5)
-#define SYM_RCTL LCTL_T(KC_6)
-#define SYM_RSFT LSFT_T(KC_7)
-
 // overrides
 const key_override_t dmrec_ko = ko_make_basic(MOD_MASK_CTRL, DM_REC1, DM_REC2);
 const key_override_t dmply_ko = ko_make_basic(MOD_MASK_CTRL, DM_PLY1, DM_PLY2);
@@ -96,20 +87,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, KC_Q   , KC_W   , KC_F   , KC_P   , KC_B,                      KC_J   , KC_L   , KC_U   , KC_Y   , KC_SCLN, XXXXXXX,
     XXXXXXX, HRGUI(KC_A), HRALT(KC_R), HRCTL(KC_S), HRSFT(KC_T), HRAGR(KC_G), HRAGR(KC_M), HRSFT(KC_N), HRCTL(KC_E), HRALT(KC_I), HRGUI(KC_O), XXXXXXX,
     XXXXXXX, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V,                      KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH, XXXXXXX,
-                                XXXXXXX, LT(_NAV, KC_ESC), LT(_MOU, KC_BSPC),    KC_ENT, LT(_SYM, KC_SPC), XXXXXXX
+                                XXXXXXX, LT(_NAV, KC_ESC), LT(_MOU, KC_BSPC),    LT(_SYM, KC_ENT), LT(_NUM, KC_SPC), XXXXXXX
+),
+
+[_NUM] = LAYOUT_split_3x6_3(
+    XXXXXXX, XXXXXXX, KC_9   , KC_8   , KC_7   , KC_EQL ,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_GRV , KC_3   , KC_2   , KC_1   , KC_LBRC,                      KC_RALT, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
+    XXXXXXX, KC_SLSH, KC_6   , KC_5   , KC_4   , KC_RBRC,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                        XXXXXXX, LT(_FUN, KC_0), KC_BSLS,      XXXXXXX, _______, XXXXXXX
 ),
 
 [_SYM] = LAYOUT_split_3x6_3(
-    XXXXXXX, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                      KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , XXXXXXX,
-    XXXXXXX, SYM_LGUI, SYM_LALT, SYM_LCTL, SYM_LSFT, KC_BSLS,                  KC_PIPE, SYM_RSFT, SYM_RCTL, SYM_RALT, SYM_RGUI, XXXXXXX,
-    XXXXXXX, KC_COLN, KC_QUES, KC_PERC, KC_EXLM , KC_GRV,                      KC_BSLS, KC_RPRN, KC_RCBR, KC_RBRC, KC_GT  , XXXXXXX,
-                                        XXXXXXX, LT(_FUN, KC_MINS), KC_UNDS,    XXXXXXX, _______, XXXXXXX
+    XXXXXXX, XXXXXXX, KC_AT  , KC_LCBR, KC_LPRN, KC_LT  ,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, KC_PERC, KC_ASTR, KC_EXLM, KC_PLUS,                      KC_RALT, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
+    XXXXXXX, XXXXXXX, KC_PIPE, KC_RCBR, KC_RPRN, KC_GT  ,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                                        XXXXXXX, KC_MINS, KC_UNDS,    _______, XXXXXXX, XXXXXXX
 ),
 
 [_NAV] = LAYOUT_split_3x6_3(
     XXXXXXX, C(KC_Z), C(KC_U), C(KC_V), C(KC_C), C(KC_X),                      C(KC_X), C(KC_C), C(KC_V), C(KC_U), C(KC_Z), XXXXXXX,
     XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_VOLU,                      KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_TAB , XXXXXXX,
-    XXXXXXX, KC_VOLD, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX,                      KC_HOME, KC_PGDN, KC_PGUP, KC_END , XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_VOLD, KC_MPRV, KC_MPLY, KC_MNXT, C(KC_A),                      KC_HOME, KC_PGDN, KC_PGUP, KC_END , XXXXXXX, XXXXXXX,
                                         XXXXXXX, _______, XXXXXXX,    KC_INS , LT(_FUN, KC_DEL) , XXXXXXX
 ),
 
@@ -121,29 +119,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_FUN] = LAYOUT_split_3x6_3(
-    XXXXXXX, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_PAUS,                      XXXXXXX,DM_PLY1, DM_REC1, DM_RSTP, XXXXXXX, XXXXXXX,
-    XXXXXXX, KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_PSCR,                      QK_BOOT, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
-    XXXXXXX, KC_F9  , KC_F10 , KC_F11 , KC_F12 , XXXXXXX,                      QK_LOCK, KC_APP , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_F10 , KC_F9  , KC_F8  , KC_F7  , KC_PAUS,                      XXXXXXX,DM_PLY1, DM_REC1, DM_RSTP, XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_F11 , KC_F3  , KC_F2  , KC_F1  , KC_PSCR,                      QK_BOOT, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
+    XXXXXXX, KC_F12 , KC_F6  , KC_F5  , KC_F4  , XXXXXXX,                      QK_LOCK, KC_APP , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                         XXXXXXX, _______, XXXXXXX,    XXXXXXX, _______, XXXXXXX
 ),
 };
-
-bool handle_sym_tap(uint16_t keycode, keyrecord_t *record) {
-    if (!record->tap.count || !record->event.pressed) return true;
-    switch (keycode) {
-        case SYM_LGUI: tap_code16(KC_AT); break;
-        case SYM_LALT: tap_code16(KC_ASTR); break;
-        case SYM_LCTL: tap_code16(KC_PLUS); break;
-        case SYM_LSFT: tap_code16(KC_EQL); break;
-
-        case SYM_RSFT: tap_code16(KC_LPRN); break;
-        case SYM_RCTL: tap_code16(KC_LCBR); break;
-        case SYM_RALT: tap_code16(KC_LBRC); break;
-        case SYM_RGUI: tap_code16(KC_LT); break;
-        default: return true;
-    }
-    return false;
-}
 
 bool ced_toggled = false;
 bool handle_ced(uint16_t keycode, keyrecord_t *record) {
@@ -189,7 +170,6 @@ bool handle_ced(uint16_t keycode, keyrecord_t *record) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
-    if (!handle_sym_tap(keycode, record)) return false;
     if (!handle_ced(keycode, record)) return false;
 
     handle_layer_stick(keycode, record);
