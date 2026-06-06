3c0be2c0dcbdcc6cd7f3eda3d13cc16e59f237c8
tbizetde@gmail.com <tbizetde@gmail.com>
2026-06-06 01:33:12 -0300

    update diagrams for NUM/SYM LLCK shift, NAV reshuffle, and GAM WASD remap

--- keyboards/crkbd/keymaps/naskbd/keymap.c
+++ keyboards/crkbd/keymaps/naskbd/keymap.c
@@ -24,9 +24,9 @@ const key_override_t* key_overrides[] = {
 };
 
 
-//   top:  B  L  D  C  V  |  J  Y  O  U  ;
-//   home: N  R  T  S  G  |  P  H  A  E  I
-//   bot:  X  Q  M  W  Z  |  K  F  ,  .  /
+//   B  L  D  C  V  |  J  Y  O  U  ;
+//   N  R  T  S  G  |  P  H  A  E  I
+//   X  Q  M  W  Z  |  K  F  ,  .  /
 
 // combos
 const uint16_t PROGMEM tab_combo[]    = {KC_L, KC_D, COMBO_END};
@@ -46,7 +46,7 @@ combo_t key_combos[] = {
     COMBO(tab_combo, KC_TAB),
     COMBO(quote_combo, KC_QUOT),
     COMBO(circ_combo, KC_CIRC),
-    COMBO(dquote_combo, KC_DQUO),
+    COMBO(dquote_combo, RSA(KC_DQUO)),
     COMBO(tilde_combo, KC_TILD),
     COMBO(hash_combo, KC_HASH),
     COMBO(ampr_combo, KC_AMPR),
@@ -102,65 +102,72 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, KC_B   , KC_L   , KC_D   , KC_C   , KC_V,                      KC_J   , KC_Y   , KC_O   , KC_U   , KC_SCLN, XXXXXXX,
     XXXXXXX, HRGUI(KC_N), HRALT(KC_R), HRCTL(KC_T), HRSFT(KC_S), HRAGR(KC_G), HRAGR(KC_P), HRSFT(KC_H), HRCTL(KC_A), HRALT(KC_E), HRGUI(KC_I), XXXXXXX,
     XXXXXXX, KC_X   , KC_Q   , KC_M   , KC_W   , KC_Z,                      KC_K   , KC_F   , KC_COMM, KC_DOT , KC_SLSH, XXXXXXX,
-                                XXXXXXX, LT(_NAV, KC_ESC), LT(_SYS, KC_BSPC),    LT(_SYM, KC_ENT), LT(_NUM, KC_SPC), XXXXXXX
+    XXXXXXX, LT(_NAV, KC_ESC), LT(_SYS, KC_BSPC),    LT(_SYM, RALT(KC_QUOT)), LT(_NUM, KC_SPC), XXXXXXX
 ),
 
 [_NUM] = LAYOUT_split_3x6_3(
     XXXXXXX, KC_GRV , KC_9   , KC_8   , KC_7   , KC_LBRC,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, KC_QUOT, KC_3   , KC_2   , KC_1   , KC_EQL ,                      KC_RALT, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
-    XXXXXXX, KC_SLSH, KC_6   , KC_5   , KC_4   , KC_RBRC,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_LLCK,
+    XXXXXXX, KC_SLSH, KC_6   , KC_5   , KC_4   , KC_RBRC,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_LLCK, XXXXXXX,
                                         XXXXXXX, LT(_FUN, KC_0), KC_BSLS,      XXXXXXX, _______, XXXXXXX
 ),
 
 [_SYM] = LAYOUT_split_3x6_3(
     XXXXXXX, KC_CIRC, KC_TILD, KC_LCBR, KC_LPRN, KC_AMPR,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, KC_PERC, KC_HASH, KC_ASTR, KC_EXLM, KC_PLUS,                      KC_RALT, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
-    XXXXXXX, KC_AT  , KC_PIPE, KC_RCBR, KC_RPRN, KC_DLR ,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_LLCK,
+    XXXXXXX, KC_AT  , KC_PIPE, KC_RCBR, KC_RPRN, KC_DLR ,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_LLCK, XXXXXXX,
                                         XXXXXXX, KC_MINS, KC_UNDS,    _______, XXXXXXX, XXXXXXX
 ),
 
 [_NAV] = LAYOUT_split_3x6_3(
-    XXXXXXX, C(KC_Z), C(KC_A), C(KC_V), C(KC_C), C(KC_X),                      C(KC_X), C(KC_C), C(KC_V), C(KC_U), KC_MUTE, XXXXXXX,
-    XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_CAPS,                      KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_VOLU, XXXXXXX,
-    QK_LLCK, TG(_MOU), KC_MPRV, KC_MPLY, KC_MNXT, C(KC_A),                      KC_HOME, KC_PGDN, KC_PGUP, KC_END , KC_VOLD, XXXXXXX,
+    XXXXXXX, C(KC_Z), C(KC_A), C(KC_V), C(KC_C), C(KC_X),                      XXXXXXX, KC_BSPC, KC_ENT, KC_TAB, XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_CAPS,                      KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, XXXXXXX,
+    XXXXXXX, QK_LLCK, KC_MUTE, KC_VOLD, KC_VOLU, TG(_MOU),                      KC_HOME, KC_PGDN, KC_PGUP, KC_END , XXXXXXX, XXXXXXX,
                                         XXXXXXX, _______, XXXXXXX,    KC_INS , LT(_FUN, KC_DEL) , XXXXXXX
 ),
+
 [_MOU] = LAYOUT_split_3x6_3(
     XXXXXXX, C(KC_Z), C(KC_U), C(KC_V), C(KC_C), C(KC_X),                      C(KC_X), C(KC_C), C(KC_V), C(KC_U), C(KC_Z), XXXXXXX,
     XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_VOLU,                      MS_LEFT, MS_DOWN, MS_UP  , MS_RGHT, KC_TAB , XXXXXXX,
     XXXXXXX, KC_VOLD, KC_MPRV, KC_MPLY, KC_MNXT, MOU_BRR,                      MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, MS_BTN3, XXXXXXX,
                                         _______, _______, _______,    MS_BTN2, MS_BTN1, XXXXXXX
 ),
+
 [_FUN] = LAYOUT_split_3x6_3(
     XXXXXXX, KC_F10 , KC_F9  , KC_F8  , KC_F7  , KC_PAUS,                      TG(_GAM), DM_PLY1, DM_REC1, DM_RSTP, XXXXXXX, TG(_GMP),
     XXXXXXX, KC_F11 , KC_F3  , KC_F2  , KC_F1  , KC_PSCR,                      QK_BOOT, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
     XXXXXXX, KC_F12 , KC_F6  , KC_F5  , KC_F4  , EE_CLR ,                      QK_LOCK, KC_APP , OSL(_PST), XXXXXXX, XXXXXXX, QK_LLCK,
                                         XXXXXXX, _______, XXXXXXX,    XXXXXXX, _______, XXXXXXX
 ),
+
 [_GAM] = LAYOUT_split_3x6_3(
-    KC_T   , KC_TAB , KC_Q   , KC_X   , KC_E   , KC_R   ,                      KC_Y, KC_U   , KC_I   , KC_O   , KC_P   , XXXXXXX,
-    KC_G   , KC_LSFT, KC_A   , KC_W   , KC_D   , KC_F   ,                      KC_H, KC_J   , KC_K   , KC_L   , KC_SCLN, XXXXXXX,
-    KC_B   , KC_LCTL, KC_Z   , KC_S   , KC_C   , KC_V   ,                      KC_N, KC_M   , KC_COMM, KC_DOT , KC_SLSH, TG(_GAM),
-                                        MO(_GNM), KC_ESC, KC_BSPC,    KC_ENT, KC_SPC , XXXXXXX
+    KC_T   , KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   ,                      KC_Y, KC_U   , KC_I   , KC_O   , KC_P   , XXXXXXX,
+    KC_G   , KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   ,                      KC_H, KC_J   , KC_K   , KC_L   , KC_SCLN, XXXXXXX,
+    KC_B   , KC_LCTL, KC_Z   , KC_X   , KC_C   , KC_V   ,                      KC_N, KC_M   , KC_COMM, KC_DOT , KC_SLSH, TG(_GAM),
+                                        MO(_GNM), KC_SPC, KC_ESC ,    KC_ENT, KC_BSPC, XXXXXXX
 ),
+
 [_GNM] = LAYOUT_split_3x6_3(
     KC_1   , KC_2   , KC_3   , XXXXXXX, KC_4   , KC_5   ,                      _______, _______, _______, _______, _______, _______,
     KC_0   , _______, _______, _______, _______, KC_6   ,                      _______, _______, _______, _______, _______, _______,
     KC_GRV , _______, KC_9   , _______, KC_8   , KC_7   ,                      _______, _______, _______, _______, _______, _______,
                                         _______, XXXXXXX, XXXXXXX,    _______, _______, _______
 ),
+
 [_GMP] = LAYOUT_split_3x6_3(
     JS_9   , JS_8   , XXXXXXX, XXXXXXX, JS_DN  , JS_LT  ,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     JS_1   , JS_3   , XXXXXXX, KC_SPC , JS_RT  , JS_UP  ,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     JS_0   , JS_2   , JS_5   , JS_7   , JS_6   , JS_4   ,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, TG(_GMP),
                                         XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_SPC , XXXXXXX
 ),
+
 [_SYS] = LAYOUT_split_3x6_3(
     XXXXXXX, G(KC_Q), K_WM9   , K_WM8   , K_WM7   , XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, C(KC_A), K_WM3   , K_WM2   , K_WM1   , XXXXXXX,                      XXXXXXX, KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, K_WM6   , K_WM5   , K_WM4   , XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                           XXXXXXX, XXXXXXX, _______,    XXXXXXX, XXXXXXX, XXXXXXX
 ),
+
 [_PST] = LAYOUT_split_3x6_3(
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, K_LKL  , K_GHL  , K_EML  , XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
@@ -287,7 +294,18 @@ bool handle_mou_exit(uint16_t keycode, keyrecord_t *record) {
     return true;
 }
 
+bool handle_ralt_quote(uint16_t keycode, keyrecord_t *record) {
+    if (keycode != LT(_SYM, KC_QUOT) || !record->tap.count) return true;
+    if (record->event.pressed) {
+        register_code(KC_RALT);
+        tap_code(KC_QUOT);
+        unregister_code(KC_RALT);
+    }
+    return false;
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+    if (!handle_ralt_quote(keycode, record)) return false;
     if (!handle_gmp_hat(keycode, record)) return false;
     if (!handle_mou_exit(keycode, record)) return false;
     if (!handle_ced(keycode, record)) return false;
--- users/NasreddinHodja/config.h
+++ users/NasreddinHodja/config.h
@@ -2,6 +2,10 @@
 
 #define PERMISSIVE_HOLD
 
+#define CHORDAL_HOLD
+
+/* #define FLOW_TAP_TERM 150 */
+
 #define QUICK_TAP_TERM 120
 #define TAPPING_TERM 180
 
